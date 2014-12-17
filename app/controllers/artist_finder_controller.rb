7e82e838167e1db4165ee6af501a43c7310fb243
pbaisla <prashant_baisla@yahoo.com>
2014-12-17 11:02:07 +0100

    Add and test lastfm gem

--- app/controllers/artist_finder_controller.rb
+++ app/controllers/artist_finder_controller.rb
@@ -1,4 +1,21 @@
 class ArtistFinderController < ApplicationController
-  def index
+  
+  @@lastfm = Lastfm.new('bca90534fbdd572bdf884e49a4d19467', '2c89fff7c7390afe5503ced33f1ca863')
+  
+  def search
+  	begin
+  		@artist = @@lastfm.artist.search(artist: params['artistsearch'])
+  	rescue Exception => e
+  		render :text => e
+  	end		
   end
+  
+  def info
+  	begin
+  		@artistinfo = @@lastfm.artist.get_info(artist: params['artist'])
+  	rescue Exception => e
+  		render :text => e
+  	end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'artist_finder#index'
-  get 'artist_finder/index'
+  root 'application#index'
+  get 'artist_finder/search'
+  get 'artist_finder/info'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
